2ef1b6a656e970f74efb4e4b55b229b16879cfe2
Larry <pudding850806@gmail.com>
2016-10-24 09:35:50 +0800

    remove some files

--- build/default.js
+++
@@ -1,3 +0,0 @@
-
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJkZWZhdWx0LmpzIiwic291cmNlUm9vdCI6InNyYyJ9
